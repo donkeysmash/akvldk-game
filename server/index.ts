2ce272606e488addc75e685027eada351f166b29
Ken Lee <ken.lee@groupbyinc.com>
2018-10-07 01:33:15 -0400

    done for the day

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,18 @@
 import * as express from 'express';
 import { SessionController } from './controllers';
+import * as path from 'path';
 
 const app: express.Application = express();
 const port: number = Number(process.env.PORT) || 3000;
 
-app.listen(port, '0.0.0.0');
+app.use(express.static(path.resolve(process.cwd(), 'dist_client')));
 
 
-// Session starts when the host initiate it
 
 app.use('/session', SessionController);
+
+
+
+app.listen(port, '0.0.0.0', () => console.log(`Server started at ${port}`));
 
 export default app;
