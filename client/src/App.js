7c1f322566570176b5c630db296cbb9a80d94220
Ken Lee <ken.lee@groupbyinc.com>
2018-10-07 15:21:01 -0400

    added nameform

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { css } from 'emotion';
 import { hot } from 'react-hot-loader';
-
-const className = css({
-  display: 'flex'
-
-});
+import NameForm from './components/NameForm';
 
 class App extends Component {
   render() {
     return (
       <div className={className}>
-        Hello World
+        <NameForm />
       </div>
     );
   }
 }
+
+const className = css({
+  display: 'flex',
+  justifyContent: 'center'
+});
 
 export default hot(module)(App);
