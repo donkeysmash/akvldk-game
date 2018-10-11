5ddf5a1b8d36bd3d5a679eb04e76690d35b2e939
Ken Lee <ken.lee@groupbyinc.com>
2018-10-10 23:11:59 -0400

    added sandbox for FE

---
+++ client/webpack.sb.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    '@babel/polyfill',
+    path.resolve(__dirname, 'src/sandbox.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, '../dist_client'),
+    filename: 'sb.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname),
+        loader: 'babel-loader'
+      }, {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    host: '0.0.0.0',
+    port: 4001,
+    historyApiFallback: true
+  }
+}
