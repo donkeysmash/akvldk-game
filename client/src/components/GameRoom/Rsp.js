89e113296b012236489dbf207fd82623e54cdb46
Kenneth Lee <ken.lee@groupbyinc.com>
2018-10-20 00:48:31 -0400

    more css

--- client/src/components/GameRoom/Rsp.js
+++ client/src/components/GameRoom/Rsp.js
@@ -24,11 +24,14 @@ class Rsp extends Component {
   render() {
     const { gameState } = this.props.gameStore;
     if (gameState.stage === SUBMIT) {
+      const rockCx = genWeaponCx(gameState, 'rock');
+      const paperCx = genWeaponCx(gameState, 'paper');
+      const scissorsCx = genWeaponCx(gameState, 'scissors');
       return (
-        <div>
-          <div className={buttonCx} onClick={this.onClickHandler.bind(this, 'rock')}>rock</div>
-          <div className={buttonCx} onClick={this.onClickHandler.bind(this, 'paper')}>paper</div>
-          <div className={buttonCx} onClick={this.onClickHandler.bind(this, 'scissors')}>scissors</div>
+        <div className={rootCx}>
+          <div className={rockCx} onClick={this.onClickHandler.bind(this, 'rock')}>✊</div>
+          <div className={paperCx} onClick={this.onClickHandler.bind(this, 'paper')}>✋</div>
+          <div className={scissorsCx} onClick={this.onClickHandler.bind(this, 'scissors')}>✌️</div>
         </div>
       )
     } else if (gameState.stage === JUDGE) {
@@ -43,7 +46,7 @@ class Rsp extends Component {
       return (
         <div>
           {listResult}
-          <div className={buttonCx} onClick={this.onReset}>reset</div>
+          <div className={resetCx} onClick={this.onReset}>reset</div>
         </div>
       );
     }
@@ -55,9 +58,27 @@ class Rsp extends Component {
   }
 }
 
+const genWeaponCx = (gameState, type) => css({
+  backgroundColor: gameState.weapon === type ? 'var(--c-teal)' : 'white'
+}, buttonCx);
+
+const rootCx = css`
+  margin-top: 1rem;
+  display: flex;
+  justify-content: space-between;
+`
+
 const buttonCx = css({
-  border: '1px purple solid',
-  margin: '1rem'
+  border: '1px var(--c-black) solid',
+  borderRadius: '0.3rem',
+  padding: '1rem',
+  fontSize: '5rem'
+});
+
+const resetCx = css({
+  border: '1px var(--c-black) solid',
+  borderRadius: '0.3rem',
+  padding: '1rem'
 });
 
 export default Rsp;
